ed1ee0dc5e083d7e93e627923d8ade6c043555e3
João Guilherme Mendanha Alves <alvesmega20@gmail.com>
2024-02-04 20:09:31 -0300

    Update PovoaTable.sql

--- PovoaTable.sql
+++ PovoaTable.sql
@@ -13,6 +13,19 @@ VALUES ('maria@e', 'Igreja Bonita', 'A igreja mais linda que já vi', 'Aparecida
 INSERT INTO postagem (EMAIL_CREATOR, NOME, DESCRICAO, CIDADE, ESTADO, LONGADOURO, PATH_IMAGEM)
 VALUES ('maria@e', 'Avenida longa', 'Que avenida longa', 'Sao Paulo', 'Sao Paulo', 'Endereço4', 'img_usuario/avenidamaria.jpg');
 
+INSERT INTO usuario (NOME, EMAIL, SENHA)
+VALUES ('Pedro', 'pedro@e', 'senha6');
+INSERT INTO postagem (EMAIL_CREATOR, NOME, DESCRICAO, CIDADE, ESTADO, LONGADOURO, PATH_IMAGEM)
+VALUES ('pedro@e', 'Um restaurante bonito', 'Melhor comida que ja comi', 'Porto Seguro', 'Bahia', 'Endereço8', 'img_usuario/imagemilustacao.png');
+INSERT INTO postagem (EMAIL_CREATOR, NOME, DESCRICAO, CIDADE, ESTADO, LONGADOURO, PATH_IMAGEM)
+VALUES ('pedro@e', 'Lagoa azul', 'Uma linda lagoa azul', 'Santos', 'Sao Paulo', 'Endereço9', 'img_usuario/imagemilustacaana');
+
+INSERT INTO usuario (NOME, EMAIL, SENHA)
+VALUES ('Ana', 'ana@e', 'senha2');
+INSERT INTO postagem (EMAIL_CREATOR, NOME, DESCRICAO, CIDADE, ESTADO, LONGADOURO, PATH_IMAGEM)
+VALUES ('ana@e', 'Loja de penhores', 'Ótimo lugar para vender e comprar coisas', 'Itabirito', 'Minas Gerais', 'Endereço10', 'img_usuario/igrejamaria.jpg');
+INSERT INTO postagem (EMAIL_CREATOR, NOME, DESCRICAO, CIDADE, ESTADO, LONGADOURO, PATH_IMAGEM)
+VALUES ('ana@e', 'Ceu no pier ', 'A noite fica lindo', 'Caldas-Novas', 'Goias', 'Endereço14', 'img_usuario/avenidamaria.jpg');
 
 
 
